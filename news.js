15505c4556de9218a4f543f2195ce0dcd5f41947
Justin Welham <jwelham@BCAA.AE>
2019-06-25 10:51:06 +0400

    news titles loaded to array

--- news.js
+++ news.js
@@ -3,11 +3,27 @@ function getNews(){
           'country=us&' +
           'apiKey=bd2270499a9b4c4890bf927eb04c8cfd';
     var req = new Request(url);
-	console.log(req);
+    console.log(req);
+    let stories = [];
     fetch(req)
         .then(resp => resp.json())
 		.then(({articles}) => {console.log(articles); return articles; })
-		.then(articles => console.log(articles["0"].author));
+        //.then(articles =>   console.log(articles["0"].title));THIS WORKS FOR ONE ARTICLE
+        .then(storyTitle => {
+            for (var prop in storyTitle){
+                if (storyTitle.hasOwnProperty(prop)){
+                    stories.push(storyTitle[prop].title);
+                    //stories.push(storyTitle["0"].title);//WORKS!!!
+                    //stories.push(storyTitle["1"].title);//WORKS!!!
+                }
+                    
+                
+            }
+            console.log(stories);
+            console.log(storyTitle.length);
+        })
+            
+        
 		
     
     }  
