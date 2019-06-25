380443cbe4ba4f3535cf843bdee4ff02f0673ccc
Justin Welham <jwelham@BCAA.AE>
2019-06-25 12:00:14 +0400

    more additions

--- news.js
+++ news.js
@@ -1,31 +1,35 @@
+let storyTitles = [];
+let storyContent = [];
+let imageURLS = [];
+
 function getNews(){
     var url = 'https://newsapi.org/v2/top-headlines?' +
           'country=us&' +
           'apiKey=bd2270499a9b4c4890bf927eb04c8cfd';
     var req = new Request(url);
     console.log(req);
-    let stories = [];
+    
     fetch(req)
         .then(resp => resp.json())
 		.then(({articles}) => {console.log(articles); return articles; })
         //.then(articles =>   console.log(articles["0"].title));THIS WORKS FOR ONE ARTICLE
-        .then(storyTitle => {
-            for (var prop in storyTitle){
-                if (storyTitle.hasOwnProperty(prop)){
-                    stories.push(storyTitle[prop].title);
+        .then(story => {
+            for (var prop in story){
+                if (story.hasOwnProperty(prop)){
+                    storyTitles.push(story[prop].title);
+                    storyContent.push(story[prop].content);
+                    imageURLS.push(story[prop].urlToImage);
                     //stories.push(storyTitle["0"].title);//WORKS!!!
                     //stories.push(storyTitle["1"].title);//WORKS!!!
                 }
                     
                 
             }
-            console.log(stories);
-            console.log(storyTitle.length);
-        })
-            
-        
-		
-    
-    }  
+            console.log(storyTitles);
+            console.log(storyContent);
+            console.log(imageURLS);
+            console.log(story.length);
+        })  
+}  
 
 
